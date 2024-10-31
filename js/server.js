8f646d6c70e595e98d309a6423de8b42e099d3df
Matt Crichton <crichton.matt@gmail.com>
2024-10-30 22:09:15 -0400

    Changed table name. Added draft schema.

--- js/server.js
+++ js/server.js
@@ -9,14 +9,34 @@ app.arguments(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/TABLE', {
+mongoose.connect('mongodb://localhost:27017/repeaters', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
 // Define schema
 const locationSchema = new mongoose.Schema({
+    state: String,
+    city: String,
+    frequency: Float32Array,
+    callsign: String,
+    offset: Int16Array,
+    notes: String,
+    id: Int32Array,
     name: String,
+    location: String,
+    modified: String,
+    type: String,
+    owner: String,
+    ori: String,
+    travel: String,
+    status: String,
+    latitude: Float32Array,
+    latitude: Float32Array,
+    network: String,
+    radius: Int16Array,
+    haat: String,
+    node: String,
     coordinates: {
         type: { type: String, enum: ['Point'], requried: true },
         coordinates: { type: [Number], required: true }
